cca83c6f16e4ecda087485dc6976e661e30a83f3
Thiago Pessoa <thiagokxl5@gmail.cpm>
2023-03-02 22:38:50 -0300

    finalizando login.

--- Agenda_Xamarin/Agenda_Xamarin/CriarAcesso.xaml.cs
+++ Agenda_Xamarin/Agenda_Xamarin/CriarAcesso.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Agenda_Xamarin.Class;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +12,40 @@ namespace Agenda_Xamarin
         public CriarAcesso()
         {
             InitializeComponent();
+        }
+
+        private async void btnCriarAcesso_Clicked(object sender, EventArgs e)
+        {
+            if (txtCriarSenha.Text != txtConfirmarSenha.Text)
+            {
+                await DisplayAlert("falha", "senha não corresponde!", "Ok");
+                return;
+            }
+            try
+            {
+                var acesso = new ServicosUser();
+                bool criarAcesso = await acesso.RegistrarUsuario(txtCriarEmail.Text, txtCriarSenha.Text);
+                if (criarAcesso)
+                {
+                    await DisplayAlert("Sucesso", "Usuario criado!", "Ok");
+                    return;
+                }
+                else
+                {
+                    await DisplayAlert("Falha", "Não foi possivel criar usuario, Tente novamente!", "Ok");
+                }
+            }
+            catch
+            {
+                await DisplayAlert("Falha", "Ocorreu um erro!", "Ok");
+            }
+        }
+
+        private void btnCancelar_Clicked(object sender, EventArgs e)
+        {
+            txtCriarEmail.Text = string.Empty;
+            txtCriarSenha.Text = string.Empty;
+            txtConfirmarSenha.Text = string.Empty;
         }
     }
 }
